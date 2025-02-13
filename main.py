1ac5ff627b06ad7db79d06ebbb3eaf39fff5e418
AlmaESL <91621109+AlmaESL@users.noreply.github.com>
2025-02-13 14:00:10 +0100

    computer + ivcam works

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+# import matlab.engine
 
 #initialize camera ports 
 cap_computer = cv2.VideoCapture(0) 
@@ -17,7 +18,7 @@ phone_window = "Phone Camera"
 stats_window = "Stats"
 
 cv2.namedWindow(computer_window)
-# cv2.namedWindow(phone_window)
+cv2.namedWindow(phone_window)
 cv2.namedWindow(stats_window)
 
 #main loop to run video frames 
@@ -25,26 +26,24 @@ while True:
 
     #read frames from computer and phone
     ret_computer, computer_frame = cap_computer.read()
-    # ret_phone, phone_frame = cap_phone.read()
+    ret_phone, phone_frame = cap_phone.read()
     
     #check that frames were captured correctly
     if not ret_computer:
         print("Failed to capture frame from computer camera.")
         break
-    # if not ret_phone:
-    #     print("Failed to capture frame from phone camera (iVCam).")
-    #     break
+    if not ret_phone:
+        print("Failed to capture frame from phone camera (iVCam).")
+        break
 
     #display frames
     cv2.imshow(computer_window, computer_frame)
-    # cv2.imshow(phone_window, phone_frame)
+    cv2.imshow(phone_window, phone_frame)
 
     #display stats on stats window 
     stats_frame = np.zeros((300, 400, 3), dtype=np.uint8)
     cv2.putText(stats_frame, "info text goes here!", (150, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.imshow(stats_window, stats_frame)
-    
-    
 
     #wait for user input to exit 
     if cv2.waitKey(1) == ord('q'):
@@ -54,5 +53,5 @@ while True:
 #-----------------------------------------------------------------------------------------------------#
 #cleanup
 cap_computer.release()
-# cap_phone.release()
+cap_phone.release()
 cv2.destroyAllWindows()
